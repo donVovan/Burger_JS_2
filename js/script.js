46f659ac09ebd411bf412cca3c85f27c732a16bc
donVovan <vladimir.a.blinov@gmail.com>
2022-11-10 22:16:33 +0300

    Бургер 3. ФИНАЛ. Рабочий вариант

--- js/script.js
+++ js/script.js
@@ -44,8 +44,8 @@ if (isMobile.any()) {
 }
 // Меню бургер
 const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
 if(iconMenu) {
-    const menuBody = document.querySelector('.menu__body');
     iconMenu.addEventListener("click", function(e) {
         document.body.classList.toggle('_lock');
         iconMenu.classList.toggle('_active');
@@ -65,6 +65,12 @@ if (menuLinks.length > 0) {
         if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
             const gotoBlock = document.querySelector(menuLink.dataset.goto);
             const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('header').offsetHeight;
+
+            if(iconMenu.classList.contains('_active')) {
+                document.body.classList.remove('_lock');
+                iconMenu.classList.remove('_active');
+                menuBody.classList.remove('_active');
+            }
 
             window.scrollTo({
                 top: gotoBlockValue,
