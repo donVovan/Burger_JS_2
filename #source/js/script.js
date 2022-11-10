19e0d47d983b5b2705f9bac69e7cd37c755446cf
donVovan <vladimir.a.blinov@gmail.com>
2022-11-10 16:37:53 +0300

    плавная навигация по разделам в JS и HTML

--- #source/js/script.js
+++ #source/js/script.js
@@ -42,3 +42,25 @@ if (isMobile.any()) {
 } else {
     document.body.classList.add('_pc');
 }
+
+// Прокрутка при клике
+
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener("click", onMenuLinkClick)
+    });
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('header').offsetHeight;
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            });
+            e.prevenDefault();
+        }
+    }
+}
